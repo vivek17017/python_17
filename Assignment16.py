62ae2eec09cf0396aa3eeb5656ea5fbde2be1cbf
Vivek Kumar <vivekgupta70509@gmail.com>
2025-04-22 01:17:03 +0530

    Add files via upload

---
+++ Assignment16.py
@@ -0,0 +1,31 @@
+#Q1
+i=1
+while i<11:
+    print(2*i)
+    i+=1
+print()
+#Q2
+i=10
+while i>0:
+    print(2*i)
+    i-=1
+print()
+#Q3
+i=1
+while i<=10:
+    print(i*i)
+    i+=1
+print()
+#Q4
+i=1
+while i<=10:
+    print(i*i*i)
+    i+=1
+print()
+#Q5
+i=1
+while i<=10:
+    print(5*i)
+    i+=1
+print()
+
